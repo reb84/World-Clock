0111db839a29b0e82c67365369f5192ce4ff9cf4
reb84 <152401972+reb84@users.noreply.github.com>
2023-12-23 19:54:23 +0000

    switched london to paris

--- src/script.js
+++ src/script.js
@@ -1,11 +1,11 @@
 function updateTime() {
-  ///london
-  let londonElement = document.querySelector("#london");
-  let londonDateElement = londonElement.querySelector(".date");
-  let londonTimeElement = londonElement.querySelector(".time");
-  let londonTime = moment().tz("Europe/London");
-  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
-  londonTimeElement.innerHTML = londonTime.format("h:mm:ss [<small>]A[</small>]");
+  ///paris
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
 
   ///singapore
   let singaporeElement = document.querySelector("#singapore");
